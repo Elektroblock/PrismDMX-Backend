6dcabf97699edcac02857935f39c9ea9b7fcd542
Elektroblock <52217834+Elektroblock@users.noreply.github.com>
2024-04-29 18:27:24 +0200

    Update settings.py

--- prismdmx/settings.py
+++ prismdmx/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fis3m1hbpwdd=7$r2i8^_)f(-mcws8&4(%28%6uf(uz6ta@4uu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.178.187']
 
 
 # Application definition
